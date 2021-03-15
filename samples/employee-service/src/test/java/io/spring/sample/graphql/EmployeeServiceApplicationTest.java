2edc4c585a843d9fa0bc33145a45506d13d4c472
Andreas Marek <andimarek@fastmail.fm>
2021-03-16 04:55:45 +1100

    add employee service example

---
+++ samples/employee-service/src/test/java/io/spring/sample/graphql/EmployeeServiceApplicationTest.java
@@ -0,0 +1,98 @@
+package io.spring.sample.graphql;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.reactive.context.ReactiveWebApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+
+import java.util.Collections;
+
+@SpringBootTest()
+class EmployeeServiceApplicationTest {
+
+    @Autowired
+    private ReactiveWebApplicationContext context;
+    private static final String BASE_URL = "https://spring.example.org/graphql";
+
+
+    WebTestClient client;
+
+    @BeforeEach
+    public void setup() {
+        this.client = WebTestClient
+                .bindToApplicationContext(this.context)
+                .apply(SecurityMockServerConfigurers.springSecurity())
+                .configureClient()
+                .filter(ExchangeFilterFunctions.basicAuthentication())
+                .defaultHeaders(headers -> {
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                    headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+                })
+                .baseUrl(BASE_URL)
+                .build();
+    }
+
+    @Test
+    void canQueryName() {
+        String query = "{" +
+                "  employees{ " +
+                "    name" +
+                "  }" +
+                "}";
+
+
+        client.post().uri("")
+                .bodyValue("{  \"query\": \"" + query + "\"}")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().jsonPath("data.employees[0].name").isEqualTo("Andi");
+
+    }
+
+    @Test
+    void canNotQuerySalary() {
+        String query = "{" +
+                "  employees{ " +
+                "    name" +
+                "    salary" +
+                "  }" +
+                "}";
+
+
+        client.post().uri("")
+                .bodyValue("{  \"query\": \"" + query + "\"}")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("data.employees[0].name").isEqualTo("Andi")
+                .jsonPath("data.employees[0].salary").doesNotExist();
+
+    }
+
+    @Test
+    @WithMockUser(roles = "MANAGER")
+    void canQuerySalaryAsManager() {
+        String query = "{" +
+                "  employees{ " +
+                "    name" +
+                "    salary" +
+                "  }" +
+                "}";
+
+
+        client.post().uri("")
+                .bodyValue("{  \"query\": \"" + query + "\"}")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("data.employees[0].name").isEqualTo("Andi")
+                .jsonPath("data.employees[0].salary").isEqualTo("42");
+
+    }
+}
