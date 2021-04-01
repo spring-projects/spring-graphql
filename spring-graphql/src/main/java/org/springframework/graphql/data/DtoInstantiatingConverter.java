2bd32511083d5aa052da869d36c3646253fa2025
Mark Paluch <mpaluch@vmware.com>
2021-04-01 12:16:34 +0200

    Add Querydsl integration
    Spring Data repositories that support Querydsl are now supported as DataFetchers returning single objects and iterables including projection support.

---
+++ spring-graphql/src/main/java/org/springframework/graphql/data/DtoInstantiatingConverter.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.graphql.data;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mapping.PersistentEntity;
+import org.springframework.data.mapping.PersistentProperty;
+import org.springframework.data.mapping.PersistentPropertyAccessor;
+import org.springframework.data.mapping.PreferredConstructor;
+import org.springframework.data.mapping.PreferredConstructor.Parameter;
+import org.springframework.data.mapping.SimplePropertyHandler;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mapping.model.EntityInstantiator;
+import org.springframework.data.mapping.model.EntityInstantiators;
+import org.springframework.data.mapping.model.ParameterValueProvider;
+
+/**
+ * {@link Converter} to instantiate DTOs from fully equipped domain objects.
+ *
+ * @author Mark Paluch
+ * @since 1.0.0
+ */
+class DtoInstantiatingConverter<T> implements Converter<Object, T> {
+
+	private final Class<T> targetType;
+
+	private final MappingContext<? extends PersistentEntity<?, ?>, ? extends PersistentProperty<?>> context;
+
+	private final EntityInstantiator instantiator;
+
+	/**
+	 * Create a new {@link Converter} to instantiate DTOs.
+	 * @param dtoType target type
+	 * @param context mapping context to be used
+	 * @param entityInstantiators the instantiators to use for object creation
+	 */
+	public DtoInstantiatingConverter(Class<T> dtoType,
+			MappingContext<? extends PersistentEntity<?, ?>, ? extends PersistentProperty<?>> context,
+			EntityInstantiators entityInstantiators) {
+		this.targetType = dtoType;
+		this.context = context;
+		this.instantiator = entityInstantiators
+				.getInstantiatorFor(context.getRequiredPersistentEntity(dtoType));
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T convert(Object source) {
+
+		if (targetType.isInterface()) {
+			return (T) source;
+		}
+
+		PersistentEntity<?, ?> sourceEntity = context
+				.getRequiredPersistentEntity(source.getClass());
+
+		PersistentPropertyAccessor<?> sourceAccessor = sourceEntity
+				.getPropertyAccessor(source);
+		PersistentEntity<?, ?> targetEntity = context
+				.getRequiredPersistentEntity(targetType);
+		PreferredConstructor<?, ? extends PersistentProperty<?>> constructor = targetEntity
+				.getPersistenceConstructor();
+
+		@SuppressWarnings({"rawtypes", "unchecked"})
+		Object dto = instantiator
+				.createInstance(targetEntity, new ParameterValueProvider() {
+
+					@Override
+					public Object getParameterValue(Parameter parameter) {
+						return sourceAccessor.getProperty(sourceEntity
+								.getRequiredPersistentProperty(parameter.getName()));
+					}
+				});
+
+		PersistentPropertyAccessor<?> dtoAccessor = targetEntity
+				.getPropertyAccessor(dto);
+
+		targetEntity.doWithProperties((SimplePropertyHandler) property -> {
+
+			if (constructor.isConstructorParameter(property)) {
+				return;
+			}
+
+			dtoAccessor.setProperty(property,
+					sourceAccessor.getProperty(sourceEntity
+							.getRequiredPersistentProperty(property.getName())));
+		});
+
+		return (T) dto;
+	}
+
+}
---
+++ spring-graphql/src/test/java/org/springframework/graphql/data/QuerydslDataFetcherSupportTests.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.graphql.data;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import com.querydsl.core.types.Predicate;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.TypeRuntimeWiring;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.repository.Repository;
+import org.springframework.graphql.execution.ExecutionGraphQlService;
+import org.springframework.graphql.execution.GraphQlSource;
+import org.springframework.graphql.web.WebGraphQlHandler;
+import org.springframework.graphql.web.WebInput;
+import org.springframework.graphql.web.WebOutput;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * Unit tests for {@link QuerydslDataFetcherSupport}.
+ */
+class QuerydslDataFetcherSupportTests {
+
+	@Test
+	void shouldFetchSingleItems() {
+		MockRepository mockRepository = mock(MockRepository.class);
+		Book book = new Book(42L, "Hitchhiker's Guide to the Galaxy", "Douglas Adams");
+		when(mockRepository.findOne(any())).thenReturn(Optional.of(book));
+
+		WebGraphQlHandler handler = initWebGraphQlHandler(builder -> builder
+				.dataFetcher("bookById", QuerydslDataFetcherSupport
+						.builder(mockRepository)
+						.single()));
+
+		WebOutput output = handler.handle(new WebInput(
+				URI.create("http://abc.org"), new HttpHeaders(), Collections
+				.singletonMap("query", "{ bookById(id: 1) {name}}"), "1")).block();
+
+		// TODO: getData interferes with method overries
+		assertThat((Object) output.getData())
+				.isEqualTo(Collections.singletonMap("bookById", Collections
+						.singletonMap("name", "Hitchhiker's Guide to the Galaxy")));
+	}
+
+	@Test
+	void shouldFetchMultipleItems() {
+		MockRepository mockRepository = mock(MockRepository.class);
+		Book book1 = new Book(42L, "Hitchhiker's Guide to the Galaxy", "Douglas Adams");
+		Book book2 = new Book(53L, "Breaking Bad", "Heisenberg");
+		when(mockRepository.findAll((Predicate) null))
+				.thenReturn(Arrays.asList(book1, book2));
+
+		WebGraphQlHandler handler = initWebGraphQlHandler(builder -> builder
+				.dataFetcher("books", QuerydslDataFetcherSupport
+						.builder(mockRepository)
+						.many()));
+
+		WebOutput output = handler.handle(new WebInput(
+				URI.create("http://abc.org"), new HttpHeaders(), Collections
+				.singletonMap("query", "{ books {name}}"), "1")).block();
+
+		assertThat((Object) output.getData())
+				.isEqualTo(Collections.singletonMap("books", Arrays.asList(Collections
+						.singletonMap("name", "Hitchhiker's Guide to the Galaxy"), Collections
+						.singletonMap("name", "Breaking Bad"))));
+	}
+
+	@Test
+	void shouldFetchSingleItemsWithInterfaceProjection() {
+		MockRepository mockRepository = mock(MockRepository.class);
+		Book book = new Book(42L, "Hitchhiker's Guide to the Galaxy", "Douglas Adams");
+		when(mockRepository.findOne(any())).thenReturn(Optional.of(book));
+
+		WebGraphQlHandler handler = initWebGraphQlHandler(builder -> builder
+				.dataFetcher("bookById", QuerydslDataFetcherSupport
+						.builder(mockRepository)
+						.projectAs(BookProjection.class)
+						.single()));
+
+		WebOutput output = handler.handle(new WebInput(
+				URI.create("http://abc.org"), new HttpHeaders(), Collections
+				.singletonMap("query", "{ bookById(id: 1) {name}}"), "1")).block();
+
+		assertThat((Object) output.getData())
+				.isEqualTo(Collections.singletonMap("bookById", Collections
+						.singletonMap("name", "Hitchhiker's Guide to the Galaxy by Douglas Adams")));
+	}
+
+	@Test
+	void shouldFetchSingleItemsWithDtoProjection() {
+		MockRepository mockRepository = mock(MockRepository.class);
+		Book book = new Book(42L, "Hitchhiker's Guide to the Galaxy", "Douglas Adams");
+		when(mockRepository.findOne(any())).thenReturn(Optional.of(book));
+
+		WebGraphQlHandler handler = initWebGraphQlHandler(builder -> builder
+				.dataFetcher("bookById", QuerydslDataFetcherSupport
+						.builder(mockRepository)
+						.projectAs(BookDto.class)
+						.single()));
+
+		WebOutput output = handler.handle(new WebInput(
+				URI.create("http://abc.org"), new HttpHeaders(), Collections
+				.singletonMap("query", "{ bookById(id: 1) {name}}"), "1")).block();
+
+		assertThat((Object) output.getData())
+				.isEqualTo(Collections.singletonMap("bookById", Collections
+						.singletonMap("name", "The book is: Hitchhiker's Guide to the Galaxy")));
+	}
+
+	@Test
+	void shouldConstructPredicateProperly() {
+		MockRepository mockRepository = mock(MockRepository.class);
+
+		WebGraphQlHandler handler = initWebGraphQlHandler(builder -> builder
+				.dataFetcher("books", QuerydslDataFetcherSupport
+						.builder(mockRepository)
+						.customizer((QuerydslBinderCustomizer<QBook>) (bindings, book) -> bindings.bind(book.name)
+								.firstOptional((path, value) -> value.map(path::startsWith)))
+						.many()));
+
+		handler.handle(new WebInput(
+				URI.create("http://abc.org"), new HttpHeaders(), Collections
+				.singletonMap("query", "{ books(name: \"H\", author: \"Doug\") {name}}"), "1")).block();
+
+
+		ArgumentCaptor<Predicate> predicateCaptor = ArgumentCaptor.forClass(Predicate.class);
+		verify(mockRepository).findAll(predicateCaptor.capture());
+
+		Predicate predicate = predicateCaptor.getValue();
+
+		assertThat(predicate).isEqualTo(QBook.book.name.startsWith("H")
+				.and(QBook.book.author.eq("Doug")));
+	}
+
+	interface MockRepository extends Repository<Book, Long>, QuerydslPredicateExecutor<Book> {
+
+	}
+
+	static WebGraphQlHandler initWebGraphQlHandler(Consumer<TypeRuntimeWiring.Builder> configurer) {
+		return WebGraphQlHandler
+				.builder(new ExecutionGraphQlService(graphQlSource(configurer)))
+				.build();
+	}
+
+	private static GraphQlSource graphQlSource(Consumer<TypeRuntimeWiring.Builder> configurer) {
+		RuntimeWiring.Builder builder = RuntimeWiring.newRuntimeWiring();
+		TypeRuntimeWiring.Builder wiringBuilder = TypeRuntimeWiring
+				.newTypeWiring("Query");
+		configurer.accept(wiringBuilder);
+		builder.type(wiringBuilder);
+		return GraphQlSource.builder()
+				.schemaResource(new ClassPathResource("books/schema.graphqls"))
+				.runtimeWiring(builder.build())
+				.build();
+	}
+
+	interface BookProjection {
+
+		@Value("#{target.name + ' by ' + target.author}")
+		String getName();
+
+	}
+
+	static class BookDto {
+
+		private final String name;
+
+		public BookDto(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return "The book is: " + name;
+		}
+	}
+
+}
