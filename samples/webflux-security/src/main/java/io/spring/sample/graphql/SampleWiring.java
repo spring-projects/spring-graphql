ed767d1a7094d51ad1aa67c387989d66bfb62a4d
Rossen Stoyanchev <rstoyanchev@vmware.com>
2021-06-25 14:06:35 +0100

    Polishing webflux-security

--- samples/webflux-security/src/main/java/io/spring/sample/graphql/SampleWiring.java
+++ samples/webflux-security/src/main/java/io/spring/sample/graphql/SampleWiring.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.spring.sample.graphql;
 
 import java.math.BigDecimal;
@@ -23,23 +38,18 @@ public class SampleWiring implements RuntimeWiringCustomizer {
 
 	@Override
 	public void customize(RuntimeWiring.Builder builder) {
-		builder.type("Query", wiringBuilder ->
-			wiringBuilder.dataFetcher("employees", env ->
-				employeeService.getAllEmployees()
-			)
+		builder.type("Query", wiring ->
+			wiring.dataFetcher("employees", env -> this.employeeService.getAllEmployees())
 		);
-		builder.type("Employee", wiringBuilder ->
-			wiringBuilder.dataFetcher("salary",  env -> {
-				Employee employee = env.getSource();
-				return salaryService.getSalaryForEmployee(employee);
-			})
+		builder.type("Employee", wiring ->
+			wiring.dataFetcher("salary",  env -> this.salaryService.getSalaryForEmployee(env.getSource()))
 		);
-		builder.type("Mutation", wiringBuilder ->
-			wiringBuilder.dataFetcher("updateSalary", env -> {
+		builder.type("Mutation", wiring ->
+			wiring.dataFetcher("updateSalary", env -> {
 				Map<String, String> input = env.getArgument("input");
 				String employeeId = input.get("employeeId");
 				BigDecimal newSalary = new BigDecimal(input.get("salary"));
-				salaryService.updateSalary(employeeId, newSalary);
+				this.salaryService.updateSalary(employeeId, newSalary);
 				return null;
 			})
 		);
--- samples/webflux-security/src/main/java/io/spring/sample/graphql/SecurityConfig.java
+++ samples/webflux-security/src/main/java/io/spring/sample/graphql/SecurityConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.spring.sample.graphql;
 
 import org.springframework.context.annotation.Bean;
@@ -20,17 +35,16 @@ public class SecurityConfig {
 	@Bean
 	SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
 		return http
-				.csrf(c -> c.disable())
+				.csrf(spec -> spec.disable())
 				// Demonstrate that method security works
 				// Best practice to use both for defense in depth
-				.authorizeExchange(requests -> requests
-					.anyExchange().permitAll()
-				)
+				.authorizeExchange(requests -> requests.anyExchange().permitAll())
 				.httpBasic(withDefaults())
 				.build();
 	}
 
 	@Bean
+	@SuppressWarnings("deprecation")
 	public MapReactiveUserDetailsService userDetailsService() {
 		User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 		UserDetails rob = userBuilder.username("rob").password("rob").roles("USER").build();
