9e8682ff8c4fa0e3abb0935d9453a4e9e2f99926
Rossen Stoyanchev <rstoyanchev@vmware.com>
2021-09-16 11:41:59 +0100

    Polishing contribution
    See gh-132

--- spring-graphql/src/main/java/org/springframework/graphql/data/method/annotation/support/ContinuationHandlerMethodArgumentResolver.java
+++ spring-graphql/src/main/java/org/springframework/graphql/data/method/annotation/support/ContinuationHandlerMethodArgumentResolver.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.graphql.data.method.annotation.support;
 
 import graphql.schema.DataFetchingEnvironment;
@@ -8,7 +23,7 @@ import org.springframework.graphql.data.method.HandlerMethodArgumentResolver;
  * No-op resolver for method arguments of type {@link kotlin.coroutines.Continuation}.
  *
  * @author Koen Punt
- * @since 5.3
+ * @since 1.0.0
  */
 public class ContinuationHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
@@ -21,4 +36,5 @@ public class ContinuationHandlerMethodArgumentResolver implements HandlerMethodA
     public Object resolveArgument(MethodParameter parameter, DataFetchingEnvironment environment) throws Exception {
         return null;
     }
+
 }
