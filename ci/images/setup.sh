31e6851f1e1174b6cc2758b5a1a6b060082d4170
Brian Clozel <bclozel@vmware.com>
2021-12-10 21:15:53 +0100

    Setup Docker support in CI
    See gh-198

--- ci/images/setup.sh
+++ ci/images/setup.sh
@@ -29,6 +29,21 @@ test -f /opt/openjdk/bin/java
 test -f /opt/openjdk/bin/javac
 
 ###########################################################
+# DOCKER
+###########################################################
+cd /
+DOCKER_URL=$( ./get-docker-url.sh )
+curl -L ${DOCKER_URL} | tar zx
+mv /docker/* /bin/
+chmod +x /bin/docker*
+
+export ENTRYKIT_VERSION=0.4.0
+curl -L https://github.com/progrium/entrykit/releases/download/v${ENTRYKIT_VERSION}/entrykit_${ENTRYKIT_VERSION}_Linux_x86_64.tgz | tar zx
+chmod +x entrykit && \
+mv entrykit /bin/entrykit && \
+entrykit --symlink
+
+###########################################################
 # GRADLE ENTERPRISE
 ###########################################################
 cd /
