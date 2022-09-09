a1165051eb53890269df33ccec402b36646e07cf
rstoyanchev <rstoyanchev@vmware.com>
2022-09-09 12:25:10 +0100

    Support method parameter annotations on interface
    Closes gh-480

--- spring-graphql/src/test/java/org/springframework/graphql/data/method/annotation/support/DataFetcherHandlerMethodTests.java
+++ spring-graphql/src/test/java/org/springframework/graphql/data/method/annotation/support/DataFetcherHandlerMethodTests.java
@@ -16,6 +16,8 @@
 package org.springframework.graphql.data.method.annotation.support;
 
 
+import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 
@@ -26,10 +28,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.graphql.data.GraphQlArgumentBinder;
 import org.springframework.graphql.data.method.HandlerMethod;
 import org.springframework.graphql.data.method.HandlerMethodArgumentResolver;
 import org.springframework.graphql.data.method.HandlerMethodArgumentResolverComposite;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.lang.Nullable;
+import org.springframework.util.ClassUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,6 +46,22 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class DataFetcherHandlerMethodTests {
 
+	@Test
+	void annotatedMethodsOnInterface() {
+
+		HandlerMethodArgumentResolverComposite resolvers = new HandlerMethodArgumentResolverComposite();
+		resolvers.addResolver(new ArgumentMethodArgumentResolver(new GraphQlArgumentBinder()));
+
+		DataFetcherHandlerMethod handlerMethod = new DataFetcherHandlerMethod(
+				handlerMethodFor(new TestController(), "hello"), resolvers, null, null, false);
+
+		Object result = handlerMethod.invoke(
+				DataFetchingEnvironmentImpl.newDataFetchingEnvironment()
+						.arguments(Collections.singletonMap("name", "Neil"))
+						.build());
+
+		assertThat(result).isEqualTo("Hello, Neil");
+	}
 
 	@Test
 	void callableReturnValue() throws Exception {
@@ -48,8 +70,8 @@ public class DataFetcherHandlerMethodTests {
 		resolvers.addResolver(Mockito.mock(HandlerMethodArgumentResolver.class));
 
 		DataFetcherHandlerMethod handlerMethod = new DataFetcherHandlerMethod(
-				new HandlerMethod(new TestController(), TestController.class.getMethod("handleAndReturnCallable")),
-				resolvers, null, new SimpleAsyncTaskExecutor(), false);
+				handlerMethodFor(new TestController(), "handleAndReturnCallable"), resolvers, null,
+				new SimpleAsyncTaskExecutor(), false);
 
 		GraphQLContext graphQLContext = new GraphQLContext.Builder().build();
 
@@ -64,8 +86,26 @@ public class DataFetcherHandlerMethodTests {
 		assertThat(future.get()).isEqualTo("A");
 	}
 
+	private static HandlerMethod handlerMethodFor(Object controller, String methodName) {
+		Method method = ClassUtils.getMethod(controller.getClass(), methodName, (Class<?>[]) null);
+		return new HandlerMethod(controller, method);
+	}
 
-	private static class TestController {
+
+	interface TestInterface {
+
+		@QueryMapping
+		String hello(@Argument String name);
+
+	}
+
+	@SuppressWarnings("unused")
+	private static class TestController implements TestInterface {
+
+		@Override
+		public String hello(String name) {
+			return "Hello, " + name;
+		}
 
 		@Nullable
 		public Callable<String> handleAndReturnCallable() {
