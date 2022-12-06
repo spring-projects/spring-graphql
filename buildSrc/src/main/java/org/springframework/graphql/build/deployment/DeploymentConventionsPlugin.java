0458f15dddf5a407279f10ff2fad22829b0c3a93
Brian Clozel <bclozel@vmware.com>
2022-12-06 11:23:39 +0100

    Move deployment repository config to a Gradle convention
    This commit moves the configuration of a local Maven deployment
    repository in a dedicated Gradle convention and applies it to all
    sub-projects. This separates the publishing configuration for actual
    project modules (that are meant to be published on Maven Central) from
    the infrastructure sub-projects such as the documentation one.

---
+++ buildSrc/src/main/java/org/springframework/graphql/build/deployment/DeploymentConventionsPlugin.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.graphql.build.deployment;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
+
+/**
+ * Convention that configures a local deployment Maven repository on the CI
+ * so that the Concourse resource can later push it to a remote repository.
+ *
+ * @author Brian Clozel
+ */
+public class DeploymentConventionsPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+		project.getPlugins().apply(MavenPublishPlugin.class);
+		project.getPlugins().withType(MavenPublishPlugin.class).forEach((mavenPublishPlugin) -> {
+			PublishingExtension publishing = project.getExtensions().getByType(PublishingExtension.class);
+			if (project.hasProperty("deploymentRepository")) {
+				publishing.getRepositories().maven((mavenArtifactRepository) -> {
+					mavenArtifactRepository.setUrl(project.property("deploymentRepository"));
+					mavenArtifactRepository.setName("deployment");
+				});
+			}
+		});
+	}
+}
