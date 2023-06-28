0beba504d7f5adc0b27c71e008a1d0df94c9257c
Babur <bduisenov@users.noreply.github.com>
2023-06-28 13:53:44 +0200

    refactor(code): Move value extractor

--- spring-graphql/src/main/java/org/springframework/graphql/data/method/annotation/support/ArgumentValueExtractor.java
+++ spring-graphql/src/main/java/org/springframework/graphql/data/method/annotation/support/ArgumentValueExtractor.java
@@ -1,8 +1,9 @@
-package org.springframework.graphql.data;
+package org.springframework.graphql.data.method.annotation.support;
 
 import jakarta.validation.valueextraction.ExtractedValue;
 import jakarta.validation.valueextraction.UnwrapByDefault;
 import jakarta.validation.valueextraction.ValueExtractor;
+import org.springframework.graphql.data.ArgumentValue;
 
 @UnwrapByDefault
 public class ArgumentValueExtractor implements ValueExtractor<ArgumentValue<@ExtractedValue ?>> {
