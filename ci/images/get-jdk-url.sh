364f0b9fd2b6f51743b284e6b0a6ea0e19405a73
Brian Clozel <bclozel@vmware.com>
2023-09-16 22:10:05 +0200

    Upgrade CI image to JDK 17.0.8

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,8 +3,7 @@ set -e
 
 case "$1" in
   java17)
-    echo "https://github.com/bell-sw/Liberica/releases/download/17.0.6+10/bellsoft-jdk17.0.6+10-linux-amd64.tar.gz"
-  ;;
+    echo "https://download.bell-sw.com/java/17.0.8.1+1/bellsoft-jdk17.0.8.1+1-linux-amd64.tar.gz"
   java19)
     echo "https://github.com/bell-sw/Liberica/releases/download/19.0.2+9/bellsoft-jdk19.0.2+9-linux-amd64.tar.gz"
     ;;
