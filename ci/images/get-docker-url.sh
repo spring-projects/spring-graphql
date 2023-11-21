bdaeb875e42453fce56d4fb2463c731b7ea5792f
Brian Clozel <bclozel@vmware.com>
2023-11-21 10:19:53 +0100

    Upgrade CI pipeline

--- ci/images/get-docker-url.sh
+++ ci/images/get-docker-url.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-version="20.10.18"
+version="24.0.7"
 echo "https://download.docker.com/linux/static/stable/x86_64/docker-$version.tgz";
--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,9 +3,9 @@ set -e
 
 case "$1" in
   java17)
-    echo "https://download.bell-sw.com/java/17.0.8.1+1/bellsoft-jdk17.0.8.1+1-linux-amd64.tar.gz"
-  java19)
-    echo "https://github.com/bell-sw/Liberica/releases/download/19.0.2+9/bellsoft-jdk19.0.2+9-linux-amd64.tar.gz"
+    echo "https://github.com/bell-sw/Liberica/releases/download/17.0.9+11/bellsoft-jdk17.0.9+11-linux-amd64.tar.gz"
+  java21)
+    echo "https://github.com/bell-sw/Liberica/releases/download/21.0.1+12/bellsoft-jdk21.0.1+12-linux-amd64.tar.gz"
     ;;
   *)
     echo $"Unknown java version"
