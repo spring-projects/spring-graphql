97be2d92e07bae54b028bd803ef2ba06745e76d2
rstoyanchev <rossen.stoyanchev@broadcom.com>
2024-05-21 09:37:22 +0100

    Upgrade to GraphQL Java 22.1
    Closes gh-975

--- spring-graphql/src/main/java/org/springframework/graphql/data/query/AutoRegistrationRuntimeWiringConfigurer.java
+++ spring-graphql/src/main/java/org/springframework/graphql/data/query/AutoRegistrationRuntimeWiringConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2023 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -159,6 +159,7 @@ class AutoRegistrationRuntimeWiringConfigurer implements RuntimeWiringConfigurer
 					objectType.getField("edges") != null && objectType.getField("pageInfo") != null);
 		}
 
+		@SuppressWarnings("deprecation")
 		private boolean hasDataFetcherFor(FieldDefinition fieldDefinition) {
 			if (this.existingQueryDataFetcherPredicate == null) {
 				Map<String, ?> map = this.builder.build().getDataFetcherForType("Query");
