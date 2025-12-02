fce1dbfd965ef973e80120878b3b9d9f61999d11
Brian Clozel <brian.clozel@broadcom.com>
2025-12-02 10:07:41 +0100

    Add Kotlin extension functions for ClientResponseField
    In gh-1331, several Kotlin extensions functions were added for
    `GraphQlClient` specs. This commit adds missing extension functions for
    `ClientResponseField`.
    
    Closes gh-1393

---
+++ spring-graphql/src/test/kotlin/org/springframework/graphql/client/GraphQlResponseFieldExtensionsTests.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.graphql.client
+
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.springframework.core.ParameterizedTypeReference
+
+
+/**
+ * Tests for [ClientResponseField] Kotlin extensions.
+ *
+ * @author Brian Clozel
+ */
+class GraphQlResponseFieldExtensionsTests {
+
+    private val responseField = mockk<ClientResponseField>(relaxed = true)
+
+    @Test
+    fun `ClientResponseField#toEntity with reified type parameters`() {
+        responseField.toEntity<Map<String, Book>>()
+        verify { responseField.toEntity(object : ParameterizedTypeReference<Map<String, Book>>() {}) }
+    }
+
+    @Test
+    fun `ClientResponseField#toEntityList with reified type parameters`() {
+        responseField.toEntityList<Book>()
+        verify { responseField.toEntityList(object : ParameterizedTypeReference<Book>() {}) }
+    }
+
+    private class Book
+
+}
